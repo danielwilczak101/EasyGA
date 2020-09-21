42e058b9eaf81a00dbe46730ecefeb2b98d6fd37
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2020-09-21 01:58:00 -0400

    Added comments

--- src/example.py
+++ src/example.py
@@ -1,15 +1,17 @@
 import random
 import EasyGA
 
+# The user defined gene function
 def user_gene_function():
     return random.randint(1, 100)
 
+# Standard user size requirements
 Population_size = 10
 Chromosome_length = 10
 
+# Create the Genetic algorithm
 ga = EasyGA.GA(Population_size, Chromosome_length,user_gene_function)
-
-# Setup the GA's population,chromosomes and genes
 ga.initialize()
 
+# Looking at the first chromosome in the population
 print(ga.population.chromosomes[0].print_chromosome())
