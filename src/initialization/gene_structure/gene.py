31f5f25c36b2c193ee506234d873786a7d26e77e
RyleyGG <griffithryley2000@gmail.com>
2020-09-27 17:46:17 -0400

    Comment updates

--- src/initialization/gene_structure/gene.py
+++ src/initialization/gene_structure/gene.py
@@ -4,21 +4,28 @@ def check_gene(value):
     return value
 
 class gene:
+
     def __init__(self, value):
+        """Initialize a gene with fitness of value None and the input value"""
         self.fitness = None
         self.value = check_gene(value)
 
     def get_fitness(self):
+        """Return fitness of the gene"""
         return self.fitness
 
     def get_value(self):
+        """Return value of the gene"""
         return self.value
 
     def set_fitness(self, fitness):
+        """Set fitness of the gene"""
         self.fitness = fitness
 
     def set_value(self):
+        """Set value of the gene"""
         self.value = value
 
     def __repr__(self):
+        """Format the repr() output value"""
         return f'[{self.value}]'
