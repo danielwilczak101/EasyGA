9c9e87141c5615a4789465f47e2a1e610bf9378a
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2020-09-28 11:58:18 -0400

    Add files via upload

---
+++ src/initialization/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from .random_initialization import random_initialization
+from .population_structure.population import population 
+from .chromosome_structure.chromosome import chromosome 
+from .gene_structure.gene import gene
