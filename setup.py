7ba39862dc8d81692e9d5900002ba592f61f841c
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2020-09-28 01:12:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='EasyGA',
-    version='0.0.12',
+    version='0.0.14',
     description='A ubiquitous or general purpuse GA',
     #py_modules=["EasyGA"],
     #package_dir={'':'src'},
     packages=find_packages(),
+    py_modules=['src'],
     python_requires='>=3.6',
     url="https://github.com/danielwilczak101/EasyGA",
     author="Daniel Wilczak",
     author_email="danielwilczak101@gmail.com",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    classifier=[
-        "Programming Language :: Python :: 3.0",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-        "Operating System :: OS Independent",
-        ],
-    install_requires = ["blessings ~= 1.7",
-                        ],
-    extra_require = {
-        "dev": [
-            "pytest>=3.7",
-            ],
-        },
+    install_requires = ["blessings ~= 1.7",],
     )
+
