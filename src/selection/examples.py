002772c71fccba6fb087a6e9180d537d80345d9e
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2020-10-01 01:22:53 -0400

    Updated comments on crossover and selection

--- src/selection/examples.py
+++ src/selection/examples.py
@@ -1,3 +1,14 @@
 class selection_examples:
-    """Selection examples will go here """
-    pass
+    """Selection defintion here"""
+
+    def tournament_selection():
+        """ """
+        pass
+
+    def roulette_selection():
+        """Roulette selection works based off of how strong the fitness is of the
+        chromosomes in the population. The stronger the fitness the higher the probability
+        that it will be selected. Using the example of a casino roulette wheel.
+        Where the chromosomes are the numbers to be selected and the board size for
+        those numbers are directly proportional to the chromosome's current fitness."""
+        pass
