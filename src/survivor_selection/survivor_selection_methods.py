e52b32f23c0f81b8a162efc7ae73624f61cb3223
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2020-10-12 17:14:24 -0400

    Fixed some errors with the new updates

--- src/survivor_selection/survivor_selection_methods.py
+++ src/survivor_selection/survivor_selection_methods.py
@@ -9,4 +9,4 @@ class Survivor_Selection:
 
     def fill_in_best(ga, next_population):
         """Fills in the next population with the best chromosomes from the last population until the population size is met."""
-        return ga.population[:ga.population_size-next_population.size()] + next_population
+        return Population(ga.population.get_all_chromosomes()[:ga.population.size()-next_population.size()] + next_population.get_all_chromosomes())
