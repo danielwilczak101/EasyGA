2f91dea637edfec2ffe20166a32fc6feb7be118e
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2020-10-13 16:05:34 -0400

    Changed testing on EasyGA

---
+++ src/test_EasyGA.py
@@ -0,0 +1,17 @@
+import random
+import EasyGA
+
+# USE THIS COMMAND WHEN TESTING -
+    # python3 -m pytest
+
+def test_chromosome_length():
+    ga = EasyGA.GA()
+    ga.chromosome_length = 100
+    ga.evolve()
+
+    """ Test to see if the actual chromosome length is the same as defined."""
+    assert ga.population.chromosome_list[0].size() == ga.chromosome_length
+
+def test_second():
+
+    assert 1 + 1 == 2
