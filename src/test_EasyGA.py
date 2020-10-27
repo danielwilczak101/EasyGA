19173bcfd2874ababa7c454c4067be1843b3a470
EREPPLab <60016577+EREPPLab@users.noreply.github.com>
2020-10-27 15:32:38 -0400

    added tests

--- src/test_EasyGA.py
+++ src/test_EasyGA.py
@@ -4,14 +4,38 @@ import EasyGA
 # USE THIS COMMAND WHEN TESTING -
     # python3 -m pytest
 
+# Tests can be broken down into three parts.
+#   - Testing correct size
+#       - Testing size while integrated with our function
+#   - Testing correct value
+#       - Testing integration with other functions
+
+def test_population_size():
+    """Test the population size is create correctly"""
+    pass
+
 def test_chromosome_length():
+    """ Test to see if the actual chromosome length is the same as defined."""
+
+    # Test from 0 to 100 chromosome length
     for i in range(0,100):
+        # Create the ga to test
         ga = EasyGA.GA()
-        ga.chromosome_length = 100
+        # Set the upper limit of testing
+        ga.chromosome_length = i
+        # Evolve the ga
         ga.evolve()
 
-        """ Test to see if the actual chromosome length is the same as defined."""
+        # If they are not equal throw an error
         assert ga.population.chromosome_list[0].size() == ga.chromosome_length
+
+def test_gene_value():
+    """ """
+    pass
+
+def test_initilization():
+    """ """
+    pass
 
 def test_second():
 
