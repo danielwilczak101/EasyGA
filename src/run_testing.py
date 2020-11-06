cb3d91d894a03d0569e68ec9a8313fc0050dc8d9
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2020-11-06 02:16:04 -0500

    Changed run testing and removed database.db file

--- src/run_testing.py
+++ src/run_testing.py
@@ -4,11 +4,18 @@ import random
 # Create the Genetic algorithm
 ga = EasyGA.GA()
 
+# Create 25 chromosomes each with 10 genes and 200 generations
+ga.population_size = 25
+ga.chromosome_length = 10
 ga.generation_goal = 200
-ga.population_size = 50
+
+# Create random genes from 0 to 10
+ga.gene_impl = lambda: random.randint(0, 10)
+
+# Minimize the sum of the genes
+ga.fitness_function_impl = lambda chromosome: sum(gene.get_value() for gene in chromosome.get_gene_list())
+ga.target_fitness_type = 'min'
 
 ga.evolve()
-
-ga.print_population()
 
 ga.graph_scatter()
