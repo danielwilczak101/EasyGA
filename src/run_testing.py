6e3402029887766926c72959a129b9ba5e7bd76d
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2020-11-12 18:37:43 -0500

    Doubled mutation rate. To solve default fitness function.

--- src/run_testing.py
+++ src/run_testing.py
@@ -7,8 +7,10 @@ ga = EasyGA.GA()
 # Create 25 chromosomes each with 10 genes and 200 generations
 ga.population_size = 100
 ga.chromosome_length = 10
-ga.generation_goal = 150
+ga.generation_goal = 1000
 
 ga.evolve()
 
-ga.graph.generation_total_fitness()
+ga.print_population()
+
+ga.graph.highest_value_chromosome()
