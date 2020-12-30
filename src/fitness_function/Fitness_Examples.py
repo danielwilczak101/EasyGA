54a774abd2d0cb874a9705739a8cda869dbfbe4a
SimpleArt <71458112+SimpleArt@users.noreply.github.com>
2020-12-30 14:35:43 -0500

    Add back files with new names

---
+++ src/fitness_function/Fitness_Examples.py
@@ -0,0 +1,46 @@
+def is_it_5(chromosome):
+    """A very simple case test function - If the chromosome's gene value
+    is equal to 5 add one to the chromosomes overall fitness value.
+    """
+
+    # Overall fitness value
+    fitness = 0
+
+    for gene in chromosome:
+
+        # Increment fitness is the gene's value is 5
+        if gene.value == 5:
+            fitness += 1
+
+    return fitness
+
+
+def near_5(chromosome):
+    """Test's the GA's ability to handle floats. Computes how close each gene is to 5."""
+
+    # Overall fitness value
+    fitness = 0
+
+    for gene in chromosome:
+
+        # Add squared distance to 5
+        fitness += (5 - gene.value) ** 2
+
+    return fitness
+
+
+def index_dependent_values(chromosome):
+    """Test of the GA's ability to improve fitness when the value is index-dependent.
+    If a gene is equal to its index in the chromosome + 1, fitness is incremented.
+    """
+
+    # Overall fitness value
+    fitness = 0
+
+    for i, gene in enumerate(chromosome):
+
+        # Increment fitness is the gene's value is i+1
+        if gene.value == i+1:
+            fitness += 1
+
+    return fitness
---
+++ src/termination_point/README.md
@@ -0,0 +1,1 @@
+# Termination functions
