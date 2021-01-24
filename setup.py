181ac18d88de6ee0336e354f3de43b14fc52d2ac
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2021-01-23 20:57:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='EasyGA',
     version='1.2.0',
     description='EasyGA is a python package designed to provide an easy-to-use Genetic Algorithm. The package is designed to work right out of the box, while also allowing the user to customize features as they see fit.',
-    py_modules=["EasyGA","attributes","test_EasyGA","decorators.py"],
+    py_modules=["EasyGA","attributes","test_EasyGA","decorators"],
     packages=find_packages(where='EasyGA'),
     package_dir={
         '': 'EasyGA',
